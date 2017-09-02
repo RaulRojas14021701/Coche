16bb8a19d4a73f1a45bde789c8419691eb1d1ad4
Carlos G. Anguiano <carlitose07@gmail.com>
2017-09-01 22:27:28 -0500

    Subido el ejemplo

---
+++ Coche/src/coche/Coche.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coche;
+
+/**
+ *
+ * @author Carlos Eduardo Gonzalez Anguiano
+ */
+public abstract class Coche {
+    private String propietario;
+    public abstract void comprar(String propietario);
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    
+}
